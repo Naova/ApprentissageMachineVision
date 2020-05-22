efa5deda47227775491f0dc3be4d9f4a717b221a
Florent Duchesne <florentduchesne@hotmail.com>
2020-05-21 23:25:12 -0400

    renommer les fichiers images et amelioration du processus

--- brut_to_png.py
+++ brut_to_png.py
@@ -3,6 +3,7 @@ from PIL import Image
 from pathlib import Path
 from tqdm import tqdm
 import config as cfg
+import os.path
 
 def main():
     dossier_entree = Path(cfg.dossier_brut).glob('**/*')
@@ -10,10 +11,12 @@ def main():
     fichiers = [str(x) for x in dossier_entree if x.is_file()]
 
     for fichier in tqdm(fichiers):
-        f = np.fromfile(fichier, dtype=np.int32)
-        f = np.reshape(f, (320,240,3))
-        image = Image.fromarray(f.astype('uint8'))
-        image.save(dossier_sortie + fichier.split('\\')[-1] + ".png", "png")
+        path_sortie = dossier_sortie + fichier.split('\\')[-1] + ".png"
+        if not os.path.isfile(path_sortie):
+            f = np.fromfile(fichier, dtype=np.int32)
+            f = np.reshape(f, (320,240,3))
+            image = Image.fromarray(f.astype('uint8'))
+            image.save(path_sortie, "png")
 
     #version en une ligne parce que je m'ennuie :
     #[Image.fromarray(np.reshape(np.fromfile(str(x), dtype=np.int32), (320,240,3)).astype('uint8')).save(cfg.dossier_PNG + str(x).split('\\')[-1] + ".png", "png") for x in Path(cfg.dossier_brut).glob('**/*') if x.is_file()]
--- dataset_update_main.py
+++ dataset_update_main.py
@@ -1,5 +1,10 @@
+import renommer_fichiers
 import brut_to_png
 import Etiquetage.main
 
+print("Renommer les fichiers")
+renommer_fichiers.main()
+print("Generation des fichiers PNG")
 brut_to_png.main()
+print("Lancement du programme d'etiquetage...")
 Etiquetage.main.main()
