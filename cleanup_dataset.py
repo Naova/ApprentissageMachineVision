c0764b7f5d6e2f6c8f7c6ffc9edd85f776e276a3
Florent Duchesne <florentduchesne@hotmail.com>
2021-02-26 13:13:37 -0500

    cleanup dataset robot

---
+++ cleanup_dataset.py
@@ -0,0 +1,24 @@
+import numpy as np
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+import config as cfg
+import os.path
+import os
+
+def cleanup(path_entree:str):
+    dossier_entree = Path(path_entree).glob('batch_*')
+    fichiers = [str(x) for x in dossier_entree if 'label' not in str(x)]
+    for fichier in tqdm(fichiers):
+        f = np.fromfile(fichier, dtype=np.float32)
+        if len(f) < 230400:
+            print(fichier)
+            #breakpoint()
+            os.remove(fichier)
+
+def main():
+    print(cfg.dossier_brut_robot)
+    cleanup(cfg.dossier_brut_robot)
+
+if __name__ == '__main__':
+    main()
