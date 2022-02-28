5f28e81f5ad5881d156ca552434bf3b87a42db85
Florent Duchesne <florentduchesne@hotmail.com>
2022-02-28 17:33:09 +0100

    script qui arrange le code autogenere

--- YOLO/fix_generated_code.py
+++ YOLO/fix_generated_code.py
@@ -48,13 +48,38 @@ def remove_next_bracket_pair(text: str):
     next_closing_bracket = text.index(']')
     return text[:next_open_bracket] + text[next_closing_bracket + 1:]
 
+def change_variable_access_method(full_text: str, variable: str):
+    var_index = full_text.index(variable+'[')
+    if variable == 'x_0':
+        shape = (*cfg.get_resized_image_resolution(), 3)
+    else:
+        second_occurence = full_text.split(variable)[2]
+        indice_name = second_occurence.split('][')[1]
+        value = int(full_text.split(indice_name+' <')[1].split(';')[0])
+        shape = (-1, value, cfg.get_nb_anchors() + 3)
+    end_of_text = full_text[var_index:]
+    next_bracket = end_of_text.index(']')
+    full_text = full_text[:var_index] + end_of_text.replace('][', f'*{shape[1]}*{shape[2]} + ', 1)
+    end_of_text = full_text[var_index:]
+    full_text = full_text[:var_index] + end_of_text.replace('][', f'*{shape[2]} + ', 1)
+    return full_text
+
 def change_arguments_to_simple_pointers(full_text: str):
     function_beginning = full_text.index('void cnn')
-    #next_bracket = full_text[function_beginning:].index('[') + function_beginning
     end_of_text = full_text[function_beginning:]
-    for _ in range(6):
+    for _ in range(3):
         end_of_text = remove_next_bracket_pair(end_of_text)
-    return full_text[:function_beginning] + end_of_text
+    next_float = end_of_text.index('float ')
+    variables_declaration = end_of_text[:next_float] + 'const '
+    x_0_index = end_of_text.index('x_0')
+    variables_declaration += end_of_text[next_float:x_0_index] + '*'
+    variables_declaration += end_of_text[x_0_index:]
+    full_text = full_text[:function_beginning] + variables_declaration
+    #a partir d'ici, la declaration de la fonction est OK
+    output_variable = end_of_text[x_0_index + 12:end_of_text.index(')')]
+    full_text = change_variable_access_method(full_text, 'x_0')
+    full_text = change_variable_access_method(full_text, output_variable)
+    return full_text
 
 def fix_file(file_path:str):
     full_text = ''
