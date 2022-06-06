96eb8e2170667564853ca9c18254a498194e2c26
Florent Duchesne <florentduchesne@hotmail.com>
2022-06-06 16:16:53 -0400

    update archive script pour images png

--- archive_dataset.py
+++ archive_dataset.py
@@ -1,10 +1,11 @@
 from tqdm import tqdm
 import zipfile
 import pathlib
+import os
 
-dataset_subfolders = ["Genere", "Robot", "Simulation", "HardNegative"]
+dataset_subfolders = ["Genere", "Robot", "Simulation", "HardNegative", "NewNegative", "TestRobot"]
 cameras = ["lower", "upper"]
-things_to_zip = ["labels.json", "Brut"]
+things_to_zip = ["labels.json", "YCbCr"]
 
 with zipfile.ZipFile('Dataset.zip', 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as archive:
     for subfolder in dataset_subfolders:
@@ -12,9 +13,10 @@ with zipfile.ZipFile('Dataset.zip', 'w', zipfile.ZIP_DEFLATED, compresslevel=9) 
             for thing_to_zip in things_to_zip:
                 s = f'Dataset/{subfolder}/{camera}/{thing_to_zip}'
                 print(s)
-                if thing_to_zip == "Brut":
-                    files = pathlib.Path(f'Dataset/{subfolder}/{camera}/Brut/').glob('**/*')
+                if thing_to_zip == "YCbCr":
+                    files = pathlib.Path(f'Dataset/{subfolder}/{camera}/YCbCr/').glob('**/*')
+                    files = [f for f in files]
                     for file in tqdm(files):
                         archive.write(str(file), arcname=str(file))
-                else:
+                elif os.path.exists(s):
                     archive.write(s)
