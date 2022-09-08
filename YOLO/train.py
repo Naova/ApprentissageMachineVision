e571c3c4fe6822c22f2e0e1aebd8946b2c071724
Florent Duchesne <florentduchesne@hotmail.com>
2022-09-08 13:38:32 -0400

    differents modeles pour simulation et robot

--- YOLO/train.py
+++ YOLO/train.py
@@ -18,7 +18,36 @@ import utils
 def kernel(x):
     return (x, x)
 
-def create_model_upper():
+def create_model_upper_simulation():
+    inputs = keras.Input(shape=(*cfg.get_resized_image_resolution(), 3))
+    x = SeparableConv2D(16, kernel(3), kernel(2), padding='same', bias_initializer='random_normal')(inputs)
+    x = LeakyReLU(alpha=0.1)(x)
+    x = SeparableConv2D(24, kernel(3), kernel(2), padding='same', bias_initializer='random_normal')(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    x = SeparableConv2D(32, kernel(3), kernel(2), padding='same', bias_initializer='random_normal')(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    x = SeparableConv2D(48, kernel(3), kernel(1), padding='same', bias_initializer='random_normal')(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    x = Conv2D(32, kernel(1), kernel(1), bias_initializer='random_normal')(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    x = Conv2D(5 + len(cfg.get_anchors()), kernel(1), kernel(1), activation='sigmoid', bias_initializer='random_normal')(x)
+    return keras.Model(inputs=inputs, outputs=x)
+
+def create_model_lower_simulation():
+    inputs = keras.Input(shape=(*cfg.get_resized_image_resolution(), 3))
+    x = SeparableConv2D(16, kernel(3), kernel(2))(inputs)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = SeparableConv2D(24, kernel(3), kernel(1), padding='same')(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = SeparableConv2D(32, kernel(3), kernel(2), padding='same')(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = MaxPool2D()(x)
+    x = Conv2D(32, kernel(1), kernel(1))(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = Conv2D(5 + len(cfg.get_anchors()), kernel(1), kernel(1), activation='sigmoid')(x)
+    return keras.Model(inputs=inputs, outputs=x)
+
+def create_model_upper_robot():
     inputs = keras.Input(shape=(*cfg.get_resized_image_resolution(), 3))
     x = SeparableConv2D(16, kernel(3), kernel(2), padding='same', bias_initializer='random_normal')(inputs)
     x = LeakyReLU(alpha=0.1)(x)
@@ -37,7 +66,7 @@ def create_model_upper():
     x = Conv2D(5 + len(cfg.get_anchors()), kernel(1), kernel(1), activation='sigmoid', bias_initializer='random_normal')(x)
     return keras.Model(inputs=inputs, outputs=x)
 
-def create_model_lower():
+def create_model_lower_robot():
     inputs = keras.Input(shape=(*cfg.get_resized_image_resolution(), 3))
     x = SeparableConv2D(64, kernel(3), kernel(2))(inputs)
     x = LeakyReLU(alpha=0.2)(x)
@@ -53,11 +82,17 @@ def create_model_lower():
     x = Conv2D(5 + len(cfg.get_anchors()), kernel(1), kernel(1), activation='sigmoid')(x)
     return keras.Model(inputs=inputs, outputs=x)
 
-def create_model():
-    if cfg.camera == 'upper':
-        return create_model_upper()
+def create_model(env):
+    if env == 'Genere':
+        if cfg.camera == 'upper':
+            return create_model_upper_robot()
+        else:
+            return create_model_lower_robot()
     else:
-        return create_model_lower()
+        if cfg.camera == 'upper':
+            return create_model_upper_simulation()
+        else:
+            return create_model_lower_simulation()
 
 def train_model(modele, train_generator, validation_generator):
     class myBinaryAccuracy(keras.metrics.BinaryAccuracy):
@@ -74,7 +109,7 @@ def train_model(modele, train_generator, validation_generator):
             return super().__call__(*args, **kwds, sample_weight=[1, 0, 0, 0, 0, 0, 0, 0])
     metrics = [myBinaryAccuracy(threshold=0.2), myFalsePositives(thresholds=0.5), myFalseNegatives(0.5), myPrecision()]
     modele.compile(optimizer=keras.optimizers.Adam(), loss='binary_crossentropy', metrics=metrics)
-    es = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=10, restore_best_weights=True, mode='max')
+    es = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=10, restore_best_weights=True)
     modele.fit(train_generator, validation_data=validation_generator, epochs=40, callbacks=[es])
     return modele
 
@@ -106,9 +141,9 @@ def generate_prediction_image(prediction, x_test, y_test, prediction_number = No
     wanted_output = utils.draw_rectangle_on_image(utils.ycbcr2rgb(x_test.copy()), y_test, coords)
     display_model_prediction(prediction[:,:,0], y_test[:,:,0], prediction_on_image, wanted_output, 'prediction_' + str(prediction_number) + '.png')
 
-def train(train_generator, validation_generator, test_data, modele_path, test=True):
+def train(train_generator, validation_generator, test_data, modele_path, env, test=True):
     if cfg.retrain:
-        modele = create_model()
+        modele = create_model(env)
         modele.summary()
         cfg.set_yolo_resolution(modele.output_shape[1], modele.output_shape[2])
         modele = train_model(modele, train_generator, validation_generator)
@@ -139,7 +174,7 @@ def main():
     if not args.simulation:
         test_data = lire_entrees('../'+cfg.get_labels_path('Robot'), '../'+cfg.get_dossier('Robot'), 'Robot')
         #test_data = lire_toutes_les_images('../'+cfg.get_dossier('RobotSansBalle'))
-    train(train_generator, validation_generator, test_data, modele_path, True)
+    train(train_generator, validation_generator, test_data, modele_path, env, True)
 
 
 if __name__ == '__main__':
