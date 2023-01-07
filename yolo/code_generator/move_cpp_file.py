898873636885cf3a1f3bb1c5a439c6d7f72e8c6c
Florent Duchesne <florentduchesne@hotmail.com>
2023-01-07 11:37:41 -0500

    change nom du fichier de modele de move cpp file

--- yolo/code_generator/move_cpp_file.py
+++ yolo/code_generator/move_cpp_file.py
@@ -9,9 +9,10 @@ def main():
     args = args_parser.parse_args_env_cam('Deplace le fichier cpp a la bonne place dans NaovaCode.')
     env = args_parser.set_config(args)
     model_path = cfg_prov.get_config().get_modele_path(env)
-    source = 'cnn_'+model_path.replace('.h5', '.cpp').replace('modele_balles', 'yolo_modele')
+    source = 'cnn_'+model_path.replace('.h5', '.cpp')
     print(source)
     destination = f'{cfg_global.naovaCodePath}/Src/Tools/NaovaTools/'
+    destination += source.replace('modele_balles', 'yolo_modele')
     print(destination)
     shutil.copy(source, destination)
 
